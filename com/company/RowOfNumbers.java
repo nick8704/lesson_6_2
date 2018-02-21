c4030b15eaecb14a3be122ef302797f86b5d33e5
Alexandr Nikonets <nick8704@gmail.com>
2018-02-21 17:50:15 +0200

    first commit

---
+++ com/company/RowOfNumbers.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class RowOfNumbers {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 15 == 0) {
+                System.out.println("HelloWorld");
+            } else if (i % 5 == 0) {
+                System.out.println("World");
+            } else if (i % 3 == 0) {
+                System.out.println("Hello");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
